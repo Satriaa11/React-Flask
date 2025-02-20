887eb6514859996ce52fb14b4f259e23f6078e55
wira <wirabkti@gmail.com>
2025-02-20 15:21:26 +0700

    feat:first-commit

---
+++ backend/models.py
@@ -0,0 +1,20 @@
+# Store database schema
+from app import db
+
+class Friend(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    role = db.Column(db.String(50), unique=False, nullable=False)
+    description = db.Column(db.Text, unique=False, nullable=False)
+    gender = db.Column(db.String(10), unique=False, nullable=False)
+    img_url = db.Column(db.String(200), unique=False, nullable=True)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'role': self.role,
+            'description': self.description,
+            'gender': self.gender,
+            'img_url': self.img_url
+        }
