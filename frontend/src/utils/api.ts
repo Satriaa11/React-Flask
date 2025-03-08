506886902c329bc37054b24f55323d82cfba43c1
wira <wirabkti@gmail.com>
2025-03-08 18:35:54 +0700

    Fix: Integration API

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import { Friends } from "../types/user";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://react-flask-production.up.railway.app/api";
 
 export const getFriends = async (): Promise<Friends[]> => {
   try {
