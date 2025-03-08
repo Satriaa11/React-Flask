9f8b1578c395d5d2a6f651bd3a751fb4103d27da
wira <wirabkti@gmail.com>
2025-03-08 16:30:17 +0700

    Fix: Add gunicorn to requirements

--- backend/app.py
+++ backend/app.py
@@ -6,8 +6,10 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-db_url = os.environ.get("DATABASE_URL", "sqlite:///instance/friends.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+if not os.path.exists("instance"):
+    os.makedirs("instance")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///instance/database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -16,8 +18,3 @@ import routes
 
 with app.app_context():
     db.create_all()
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
