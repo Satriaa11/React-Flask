c870ab2a262b40d942afde5ea67cc2112aa995d5
wira <wirabkti@gmail.com>
2025-03-08 15:06:14 +0700

    Prepare Flask app for deployment

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/friends.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -16,5 +17,12 @@ import routes
 with app.app_context():
     db.create_all()
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# Upload Render Server
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Gunakan port dari environment variable
+    app.run(host="0.0.0.0", port=port)
